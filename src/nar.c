6c7c285ce25475994e82705219b4f4594b7ac6e5
ashtneoi <ashtneoi@gmail.com>
2022-08-21 17:39:12 -0700

    Implement NAR serialization, except for contents
    Development assisted by sentient potato.

--- src/nar.c
+++ src/nar.c
@@ -5,6 +5,7 @@
 #include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -73,6 +74,8 @@
 #define MIN(a, b) ((a) <= (b) ? (a) : (b))
 
 
+void serialize_prime(const char* path);
+
 const char* errno_name(int err)
 {
     switch (err) {
@@ -84,6 +87,7 @@ const char* errno_name(int err)
         case ENOENT: return "ENOENT";
         case ENOMEM: return "ENOMEM";
         case ENOTDIR: return "ENOTDIR";
+        case ENOTTY: return "ENOTTY";
         case EOVERFLOW: return "EOVERFLOW";
         default: return NULL;
     }
@@ -94,57 +98,124 @@ int dirent_name_strcmp(const struct dirent** a, const struct dirent** b)
     return strcmp((*a)->d_name, (*b)->d_name);
 }
 
-void scandir_test(const char* path)
+void serialize_u64(uint64_t n)
 {
-    struct dirent** names; // scandir initializes this
-    int names_len = scandir(path, &names, NULL, dirent_name_strcmp);
-    E_EXPECTF(names_len >= 0, "scandir failed on \"%s\"", path);
-
-    int dir = open(path, O_RDONLY);
-    E_EXPECTF(dir >= 0, "open failed on \"%s\"", path);
-
-    for (int i = 0; i < names_len; i++) {
-        if (0 == strcmp(names[i]->d_name, ".") || 0 == strcmp(names[i]->d_name, "..")) {
-            continue;
-        }
-
-        struct stat stat_buf;
-        E_EXPECT(0 == fstatat(dir, names[i]->d_name, &stat_buf, AT_SYMLINK_NOFOLLOW));
-
-        char t;
-        switch (stat_buf.st_mode & S_IFMT) { // file type
-            case S_IFREG: t = 'r'; break;
-            case S_IFLNK: t = 's'; break;
-            case S_IFDIR: t = 'd'; break;
-            default: t = '?'; break;
-        }
-
-        char p;
-        if (!(stat_buf.st_mode & S_IFDIR) && stat_buf.st_mode & S_IXUSR) {
-            p = 'x';
-        } else {
-            p = '-';
-        }
-
-        printf("%s/%s %c%c\n", path, names[i]->d_name, t, p);
-
-        if (S_ISDIR(stat_buf.st_mode)) {
-            char* dir_path = malloc(strlen(path) + 1 + strlen(names[i]->d_name) + 1);
-            E_EXPECT(dir_path);
-            sprintf(dir_path, "%s/%s", path, names[i]->d_name);
-            scandir_test(dir_path);
-        }
+    for (int i = 0; i < 8; i++) {
+        uint8_t b = n & 0xFF;
+        n >>= 8;
+        E_EXPECTF(1 == write(STDOUT_FILENO, &b, 1), "write failed");
     }
+}
 
-    for (int i = 0; i < names_len; i++) {
-        free(names[i]);
+void serialize_str(const char* s)
+{
+    size_t remaining = strlen(s); // don't write the null terminator
+    serialize_u64(remaining);
+    while (remaining > 0) {
+        ssize_t count = write(STDOUT_FILENO, s, remaining);
+        E_EXPECTF(count >= 0, "write failed");
+        remaining -= (size_t)count;
+        s += count;
     }
-    free(names);
+    // TODO: pad it!
+}
+
+// The caller must free the returned string.
+char* path_join(const char* a, const char* b)
+{
+    char* joined = malloc(strlen(a) + 1 + strlen(b) + 1);
+    E_EXPECT(joined);
+    int a_is_root = a[0] == '/' && a[1] == 0;
+    sprintf(joined, "%s/%s", a_is_root ? "" : a, b);
+    return joined;
+}
+
+void serialize_entry(const char* path, const char* name)
+{
+    serialize_str("entry");
+    serialize_str("(");
+    serialize_str("name");
+    serialize_str(name);
+    serialize_str("node");
+    char* child_path = path_join(path, name);
+    serialize_prime(child_path);
+    free(child_path);
+    serialize_str(")");
+}
+
+void serialize_prime_prime(const char* path)
+{
+    struct stat stat_buf;
+    E_EXPECT(0 == lstat(path, &stat_buf));
+
+    serialize_str("type");
+    switch (stat_buf.st_mode & S_IFMT) { // file type
+        case S_IFREG:
+            serialize_str("regular");
+            if (stat_buf.st_mode & S_IXUSR) {
+                serialize_str("executable");
+                serialize_str("");
+            }
+            serialize_str("contents");
+            serialize_str("TODO"); // TODO
+            break;
+        case S_IFLNK:
+            serialize_str("symlink");
+            serialize_str("target");
+            serialize_str("TODO"); // TODO
+            break;
+        case S_IFDIR:
+            serialize_str("directory");
+
+            struct dirent** names; // scandir allocates the list and its contents
+            int names_len = scandir(path, &names, NULL, dirent_name_strcmp);
+            E_EXPECTF(names_len >= 0, "scandir failed on \"%s\"", path);
+
+            int dir = open(path, O_RDONLY);
+            E_EXPECTF(dir >= 0, "open failed on \"%s\"", path);
+
+            for (int i = 0; i < names_len; i++) {
+                if (0 == strcmp(names[i]->d_name, ".") || 0 == strcmp(names[i]->d_name, "..")) {
+                    continue;
+                }
+
+                serialize_entry(path, names[i]->d_name);
+            }
+
+            for (int i = 0; i < names_len; i++) {
+                free(names[i]);
+            }
+            free(names);
+            break;
+        default:
+            fprintf(stderr, "Error: file \"%s\" has unknown or illegal type", path);
+            exit(1);
+    }
+}
+
+void serialize_prime(const char* path)
+{
+    serialize_str("(");
+    serialize_prime_prime(path);
+    serialize_str(")");
+}
+
+void serialize(const char* path)
+{
+    serialize_str("nix-archive-1");
+    serialize_prime(path);
 }
 
 int main(int argc, char** argv)
 {
     EXPECT(argc == 2);
+
+    int is_tty = isatty(STDOUT_FILENO);
+    E_EXPECT(is_tty >= 0);
+    if (is_tty) {
+        fprintf(stderr, "Error: stdout is a TTY but NAR archives are binary data\n");
+        exit(1);
+    }
 
     char* path = argv[1];
     size_t path_len = strlen(path);
@@ -152,5 +223,6 @@ int main(int argc, char** argv)
         path[path_len - 1] = 0;
         path_len--;
     }
-    scandir_test(path);
+    EXPECT(strlen(path) != 0);
+    serialize(path);
 }
